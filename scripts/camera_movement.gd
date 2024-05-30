0455ffd02ebccf35f1690ce0aecac35c0bd95d54
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-05-30 23:21:58 +0300

    connect input_screen with map_generator scene, add camera movements to map_generator scene

---
+++ scripts/camera_movement.gd
@@ -0,0 +1,58 @@
+extends Camera2D
+
+@export var zoomSpeed : float = 10;
+
+var zoomTarget :Vector2
+
+var dragStartMousePos = Vector2.ZERO
+var dragStartCameraPos = Vector2.ZERO
+var isDragging : bool = false
+
+func _ready():
+	zoomTarget = zoom
+
+
+func _process(delta):
+	Zoom(delta)
+	SimplePan(delta)
+	ClickAndDrag()
+	
+func Zoom(delta):
+	if Input.is_action_just_pressed("camera_zoom_in"):
+		zoomTarget *= 1.1
+		
+	if Input.is_action_just_pressed("camera_zoom_out"):
+		zoomTarget *= 0.9
+		
+	zoom = zoom.slerp(zoomTarget, zoomSpeed * delta)
+	
+	
+func SimplePan(delta):
+	var moveAmount = Vector2.ZERO
+	if Input.is_action_pressed("camera_move_right"):
+		moveAmount.x += 1
+		
+	if Input.is_action_pressed("camera_move_left"):
+		moveAmount.x -= 1
+		
+	if Input.is_action_pressed("camera_move_up"):
+		moveAmount.y -= 1
+		
+	if Input.is_action_pressed("camera_move_down"):
+		moveAmount.y += 1
+		
+	moveAmount = moveAmount.normalized()
+	position += moveAmount * delta * 1000 * (1/zoom.x)
+	
+func ClickAndDrag():
+	if !isDragging and Input.is_action_just_pressed("camera_pan"):
+		dragStartMousePos = get_viewport().get_mouse_position()
+		dragStartCameraPos = position
+		isDragging = true
+		
+	if isDragging and Input.is_action_just_released("camera_pan"):
+		isDragging = false
+		
+	if isDragging:
+		var moveVector = get_viewport().get_mouse_position() - dragStartMousePos
+		position = dragStartCameraPos - moveVector * 1/zoom.x
--- scripts/map_generator.gd
+++ scripts/map_generator.gd
@@ -2,14 +2,9 @@ extends TileMap
 
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new() # random number generator
-
-
 # Tiles coordinates based on medieval_tilesheet (TileSet)
 const village_1_tile_coords: Dictionary = {'x': 5, 'y': 6}
 const village_2_tile_coords: Dictionary = {'x': 7, 'y': 6}
-
-const villager_1_tile_coords: Dictionary =  {'x': 12, 'y': 5}
-const villager_2_tile_coords: Dictionary =  {'x': 12, 'y': 4}
 
 const stone_tile_coords: Dictionary = {'x': 7, 'y': 4}
 const gold_tile_coords: Dictionary = {'x': 9, 'y': 5}
@@ -22,34 +17,45 @@ const top_right_corner_boundary_tile_coords: Dictionary = {'x': 5, 'y': 1}
 const bottom_left_corner_boundary_tile_coords: Dictionary = {'x': 4, 'y': 2}
 const bottom_right_corner_boundary_tile_coords: Dictionary = {'x': 5, 'y': 2}
 
+# Input fields
+static var rows: int; # rows
+static var cols: int; # columns
+static var stone: int; # number of gold resources
+static var wood: int; # number of wood resources 
+static var gold: int; # number of gold resources
+static var agents: int; # number of agents for each village
+
+static func set_input_arguments(
+	rows_arg: int, cols_arg: int, stone_arg: int,
+	wood_arg: int, gold_arg: int, agents_arg: int
+):
+	rows = rows_arg
+	cols = cols_arg
+	stone = stone_arg
+	wood = wood_arg
+	gold = gold_arg
+	agents = agents_arg
+
 
 func _ready():
 	randomize() # set a random seed for the RandomNumberGenerator
 	
-	# Input fields
-	const N: int = 15 # rows
-	const M: int = 29 # columns
-	var stone: int = 5 # number of gold resources
-	var wood: int = 6 # number of wood resources 
-	var gold: int = 3 # number of gold resources
-	var K: int = 4 # number of agents for each village
-	
 	# Initialize a 2D array representing the map
 	var map: Array = []
-	var available_rows: Array = range(1, N-1)
-	for y in N:
+	var available_rows: Array = range(1, rows-1)
+	for y in rows:
 		map.append([])
-		for x in range(1, M-1):
+		for x in range(1, cols-1):
 			map[y].append(x)
 	
 	# Tiles placement into the map
 	add_tile_into_the_map(1, map, available_rows, village_1_tile_coords)
 	add_tile_into_the_map(1, map, available_rows, village_2_tile_coords)
 	
-	for y in N:
-		for x in M:
+	for y in rows:
+		for x in cols:
 			set_cell(0, Vector2i(x, y), 0, Vector2i(0, 0)) # grass placement
-			add_boundary_in_the_map(x, y, N, M) # boundary placement
+			add_boundary_in_the_map(x, y) # boundary placement
 			if stone > 0: # stone placement
 				add_tile_into_the_map(1, map, available_rows, stone_tile_coords)
 				stone -= 1
@@ -59,9 +65,9 @@ func _ready():
 			if wood > 0: # wood placement
 				add_tile_into_the_map(1, map, available_rows, wood_tile_coords)
 				wood -= 1
-			if K > 0: # agents placement
+			if agents > 0: # agents placement
 				# Todo: Agents placement
-				K -= 1 
+				agents -= 1 
 
 func add_tile_into_the_map(layer: int, map: Array, available_rows: Array,
 				tile_coords: Dictionary):
@@ -77,7 +83,7 @@ func add_tile_into_the_map(layer: int, map: Array, available_rows: Array,
 
 	set_cell(1, Vector2i(x, y), 0, Vector2i(tile_coords.x, tile_coords.y))
 
-func add_boundary_in_the_map(x: int, y: int, N: int, M: int):
+func add_boundary_in_the_map(x: int, y: int):
 	"""
 		This function adds a boundary to the map.
 	"""
@@ -85,23 +91,23 @@ func add_boundary_in_the_map(x: int, y: int, N: int, M: int):
 		set_cell(1, Vector2i(x, y),
 					0, Vector2i(top_left_corner_boundary_tile_coords.x,
 								top_left_corner_boundary_tile_coords.y))
-	elif x == M-1 and y == 0: # top-right corner
+	elif x == cols-1 and y == 0: # top-right corner
 		set_cell(1, Vector2i(x, y),
 					0, Vector2i(top_right_corner_boundary_tile_coords.x,
 								top_right_corner_boundary_tile_coords.y))
-	elif x == 0 and y == N-1: # bottom-left corner
+	elif x == 0 and y == rows-1: # bottom-left corner
 		set_cell(1, Vector2i(x, y),
 					0, Vector2i(bottom_left_corner_boundary_tile_coords.x,
 								bottom_left_corner_boundary_tile_coords.y))
-	elif x == M-1 and y == N-1: # bottom-right corner
+	elif x == cols-1 and y == rows-1: # bottom-right corner
 		set_cell(1, Vector2i(x, y),
 					0, Vector2i(bottom_right_corner_boundary_tile_coords.x,
 								bottom_right_corner_boundary_tile_coords.y))
-	elif x == 0 or x == M-1: # first/last column
+	elif x == 0 or x == cols-1: # first/last column
 		set_cell(1, Vector2i(x, y),
 					0, Vector2i(vertical_boundary_tile_coords.x,
 								vertical_boundary_tile_coords.y))
-	elif y == 0 or y == N-1: # first/last row
+	elif y == 0 or y == rows-1: # first/last row
 		set_cell(1, Vector2i(x, y),
 					0, Vector2i(horizontal_boundary_tile_coords.x,
 								horizontal_boundary_tile_coords.y))

858907c30a576c28a465f48d9277a6d8eb748f93
Dimitris Stylianou <dimitrisstylianou@proton.me>
2024-06-06 15:47:43 +0300

    Update resource.gd
    - Remove resources dictionary.
    - Remove init values for type, TOTAL_AMOUNT and current_amount variables.
    - Change TOTAL_AMOUNT from const to var.
    - Rename TOTAL_AMOUNT variable to total_quantity.
    - Rename current_amount variable to current_quantity.
    - Rename other references of 'amount' with 'quantity'.
    - Create setter function set_total_quantity.

--- scripts/resource.gd
+++ scripts/resource.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
-var resources = {
-	"stone": $StoneSprite,
-	"wood": $WoodSprite,
-	"gold": $GoldSprite
-}
-
 @onready var label = $Label
 
-var type := "stone"
-const TOTAL_AMOUNT = 11
-var current_amount := TOTAL_AMOUNT
+var type: String
+var total_quantity: int
+var current_quantity: int
+
+
+func set_total_quantity(quantity: int) -> void:
+	self.total_quantity = quantity
+	self.current_quantity = quantity
+
 
 func _on_ready():
 	update_label()
@@ -18,11 +18,11 @@ func _on_ready():
 func _on_body_entered(body):
 	body.on_resource_interact(self)
 
-func loot(amount):
-	var amount_to_return = amount if current_amount >= amount else current_amount
-	current_amount -= amount_to_return
+func loot(quantity):
+	var quantity_to_return = quantity if current_quantity >= quantity else current_quantity
+	current_quantity -= quantity_to_return
 	update_label()
-	return amount_to_return
+	return quantity_to_return
 
 func update_label():
-	label.text = str(current_amount) + "/" + str(TOTAL_AMOUNT)
+	label.text = str(current_quantity) + "/" + str(total_quantity)

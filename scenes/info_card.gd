6c1f47a8958a730c505f929cb467845f469959ae
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-07 17:47:43 +0300

    fix the zoom/gui error with tilemap

--- scenes/info_card.gd
+++ scenes/info_card.gd
@@ -18,9 +18,17 @@ func _on_timer_timeout():
 
 func update_info():
 	if agent != null:
+		
+		# Convert the state enum to a string
+		var state_string = ""
+		match agent.current_state:
+			agent.State.WALKING: state_string = "WALKING"
+			agent.State.DECIDING: state_string = "DECIDING"
+			agent.State.REFILLING: state_string = "REFILLING"
+			agent.State.IDLE: state_string = "IDLE"
+		label_state.text = "State: " + state_string
 		label_agent_id.text = "Agent ID: " + str(agent.get_name())
 		label_agent_village.text = "Village: " + str(agent.village_type)
-		label_state.text = "State: " + str(agent.current_state)
 		label_energy.text = "Energy: " + str(agent.energy)
 		if agent.current_carrying_resource:
 			label_resource.text = "Resource: " + str(agent.current_carrying_resource.type) + " (" + str(agent.current_carrying_resource.quantity) + ")"
